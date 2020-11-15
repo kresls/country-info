1ca2852eea3f8df243a70b779da01f181dc3feaa
Filip Zalamans <filip.zalamans@gmail.com>
2020-11-15 20:14:24 +0100

    Add public holidays widget

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 
-
 let timeouts = [];
 
 function ClearAllTimeouts() {
@@ -33,7 +32,7 @@ class SearchBar extends React.Component {
       suggestionsFound: false,
       countryPicked: {
         status: false,
-        data: {}
+        country: {}
       }
     }
   }
@@ -160,13 +159,87 @@ class WidgetContainer extends React.Component {
       country: this.props.country
     }
   }
-  componentDidMount() {
-    console.log(this.state.country)
+  componentDidUpdate(prevProps) {
+    if(prevProps.country !== this.props.country) {
+      this.setState({
+        country: this.props.country
+      })
+    }
   }
   render() {
     return (
-      <div>The widgets go here</div>
+      <div>
+        <Holidays code={this.state.country.alpha2Code} name={this.state.country.name} />
+      </div>
     )
+  }
+}
+
+class Holidays extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      holidaysFound: false,
+      holidays: {}
+    }
+  }
+  componentDidMount() {
+    this.fetch()
+  }
+  componentDidUpdate(prevProps) {
+    if(prevProps.code !== this.props.code) {
+      this.fetch()
+    }
+  }
+  handleResult = (result) => {
+    if(!result.status) { // Results found
+      this.setState({
+        holidaysFound: true,
+        holidays: result
+      })
+    }
+    else { // No results
+      this.setState({
+        holidaysFound: false,
+        holidays: {}
+      })
+    }
+  }
+  fetch() {
+    fetch(
+      "https://date.nager.at/api/v2/publicholidays/" + new Date().getFullYear() + "/" + this.props.code)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.handleResult(result)
+        }
+      )
+  }
+  render() {
+    if(this.state.holidaysFound) {
+      return (
+        <div>
+          <h4>{new Date().getFullYear()} public holidays in {this.props.name}</h4>
+        {this.state.holidays.map(function(holiday, index) {
+          return (
+            <div
+              key={index}
+              className="holiday-container">
+                <span className="holiday-name">{holiday.name}</span><br />
+                {holiday.name !== holiday.localName ? <span className="holiday-localName">{holiday.localName}<br /></span> : null}
+                <span className="holiday-date">{holiday.date}</span><br /><br />
+            </div>
+          )
+        }, this)}
+        </div>
+      )
+    }
+    else {
+      return (
+          null
+        )
+    }
   }
 }
 
